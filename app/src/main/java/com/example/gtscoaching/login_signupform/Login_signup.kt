e3ff2d312102a2bc8916469c88120b2cea90f60e
sidhartha8011 <sidharthparasramka@gmail.com>
2023-04-06 16:19:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gtscoaching/login_signupform/Login_signup.kt
@@ -0,0 +1,12 @@
+package com.example.gtscoaching.login_signupform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gtscoaching.R
+
+class login_signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_signup)
+    }
+}
